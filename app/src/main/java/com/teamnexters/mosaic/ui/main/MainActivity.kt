e466a96e467de98325bd40a5c3a0dd345b5f3e3a
OH JAE HWAN <ojh102@gmail.com>
2018-08-06 22:04:19 +0900

    메인화면 변경으로 인한 하단탭 및 프래그먼트들의 사-망

--- app/src/main/java/com/teamnexters/mosaic/ui/main/MainActivity.kt
+++ app/src/main/java/com/teamnexters/mosaic/ui/main/MainActivity.kt
@@ -1,19 +1,11 @@
 package com.teamnexters.mosaic.ui.main
 
 import android.os.Bundle
-import android.support.design.widget.BottomNavigationView
-import android.view.MenuItem
 import com.teamnexters.mosaic.R
 import com.teamnexters.mosaic.base.BaseActivity
 import com.teamnexters.mosaic.databinding.ActivityMainBinding
-import com.teamnexters.mosaic.ui.main.home.HomeFragment
-import com.teamnexters.mosaic.ui.main.mypage.MyPageFragment
-import com.teamnexters.mosaic.utils.Navigator
-import com.teamnexters.mosaic.utils.extension.addFragment
-import com.teamnexters.mosaic.utils.extension.hideFragment
-import com.teamnexters.mosaic.utils.extension.showFragment
 
-class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(), BottomNavigationView.OnNavigationItemSelectedListener {
+class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>() {
 
     override fun getLayoutRes() = R.layout.activity_main
 
@@ -25,57 +17,10 @@ class MainActivity : BaseActivity<ActivityMainBinding, MainViewModel>(), BottomN
         binding.viewModel = viewModel
 
         initializeToolbar()
-        initializeBottomNavigation()
-        initializeFragments()
-    }
-
-    override fun onNavigationItemSelected(item: MenuItem): Boolean {
-        return when(item.itemId) {
-            R.id.navigation_home -> {
-                hideFragment(MyPageFragment.TAG)
-                showFragment(HomeFragment.TAG)
-
-                true
-            }
-            R.id.navigation_upload -> {
-                Navigator.navigateToWrite(this)
-
-                false
-            }
-            R.id.navigation_mypage -> {
-                hideFragment(HomeFragment.TAG)
-                showFragment(MyPageFragment.TAG)
-
-                true
-            }
-            else -> {
-                false
-            }
-        }
     }
 
     private fun initializeToolbar() {
 
-    }
-
-    private fun initializeBottomNavigation() {
-        binding.navigation.run {
-            enableAnimation(false)
-            enableShiftingMode(false)
-            enableItemShiftingMode(false)
-            setTextVisibility(false)
-
-            elevation = 0f
-            itemIconTintList = null
-            onNavigationItemSelectedListener = this@MainActivity
-        }
-    }
-
-    private fun initializeFragments() {
-        addFragment(R.id.container_fragment, HomeFragment.newInstance(), HomeFragment.TAG)
-        addFragment(R.id.container_fragment, MyPageFragment.newInstance(), MyPageFragment.TAG)
-
-        hideFragment(MyPageFragment.TAG)
     }
 
 }
--- app/src/main/java/com/teamnexters/mosaic/ui/main/mypage/MyPageViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.teamnexters.mosaic.ui.main.mypage
-
-import com.teamnexters.mosaic.base.BaseViewModel
-import javax.inject.Inject
-
-class MyPageViewModel @Inject constructor() : BaseViewModel() {
-
-}
